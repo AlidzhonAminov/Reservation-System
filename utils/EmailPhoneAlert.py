85272eaabab3a44a4924784f86040984002d4f1b
AlidzhonAminov <106463097+AlidzhonAminov@users.noreply.github.com>
2022-11-13 23:12:48 +0500

    EmailPhoneAlert.py contains functions that send users EMAILS and SMS … …notifications about reservations.

--- utils/EmailPhoneAlert.py
+++ utils/EmailPhoneAlert.py
@@ -5,7 +5,7 @@ import json
 import smtplib
 
 
-def send_email(recipient: str, room_num: int, made_by: str, reservation_id: int, from_datetime: str, until_datetime: str) -> None:
+def send_email(recipient: str, room_num: str, made_by: str, reservation_id: int, from_datetime: str, until_datetime: str) -> None:
     """
     This function sends an email notification about a reservation.
     :param recipient: user's email address for sending an email notification about a reservation
@@ -39,9 +39,9 @@ def send_email(recipient: str, room_num: int, made_by: str, reservation_id: int,
         print("Failed to send mail")
 
 
-def send_sms(recipient: str, room_num: int, made_by: str, reservation_id: int, from_datetime: str, until_datetime:str) -> None:
+def send_sms(recipient: str, room_num: str, made_by: str, reservation_id: int, from_datetime: str, until_datetime:str) -> None:
     """
-    This function sends an SMS notification about a reservation.
+    This function sends an SMS notification about a reservation.,
     :param recipient: user's phone number for sending a message notification about a reservation
     :param room_num: a room number of the room that a user wants to reserve
     :param made_by: user's name
@@ -49,15 +49,13 @@ def send_sms(recipient: str, room_num: int, made_by: str, reservation_id: int, f
     :param from_datetime: from what time and date is a room reserved by a user
     :param until_datetime: until what time and date the room is reserved by user
     :return: None
-
     """
-
     load_dotenv()
     try:
         service_plan_id = os.environ['service_plan_id']
-        access_token  = os.environ['access_token']
+        access_token = os.environ['access_token']
         FROM = '447520651206'
-        TO = recipient
+        TO = '992' + recipient
         URL = f'https://sms.api.sinch.com/xms/v1/{service_plan_id}/batches'
 
         header = {
@@ -65,13 +63,15 @@ def send_sms(recipient: str, room_num: int, made_by: str, reservation_id: int, f
             'Content-Type': 'application/json'
         }
         message_info = {
-            'from':FROM,
+            'from': FROM,
             'to': [TO],
             'body': f'Reservation ID: {reservation_id}\n' \
-               f'Hello {made_by}, you have reserved the the room {room_num}\n' \
-               f'From: {from_datetime} ~ Until: {until_datetime}\n'\
+                    f'Hello {made_by}, you have reserved the the room {room_num}\n' \
+                    f'From: {from_datetime} ~ Until: {until_datetime}\n'\
             }
-    except:
-        print("Failed to send sms")
+        requests.post(URL, headers=header, data=json.dumps(message_info))
 
-    requests.post(URL, headers=header, data = json.dumps(message_info))
+    except:
+        print("Failed to send SMS")
+
+
